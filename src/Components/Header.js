7184715865441b07f1d46a64599444149edd763f
kimfriendship <kfriendship94@gmail.com>
2020-12-16 23:06:31 +0900

    :lipstick: Add styles on header and footer

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classNames from "classnames/bind";
 import styles from "../Scss/app.scss";
 
 const cx = classNames.bind(styles);
 
 const Header = () => {
-  return <header className={cx("header")}>header</header>;
+  return (
+    <header className={cx("header")}>
+      <h1 className={cx("logo")}>kimfriendship</h1>
+      <nav>
+        <NavLink
+          to="/"
+          exact
+          activeClassName={cx("activeMenu")}
+          className={cx("menu")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/project"
+          activeClassName={cx("activeMenu")}
+          className={cx("menu")}
+        >
+          Project
+        </NavLink>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
