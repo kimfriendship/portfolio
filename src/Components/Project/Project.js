e6f76b387427585978f09e4cb9633e289880cf13
kimfriendship <kfriendship94@gmail.com>
2020-12-24 00:44:52 +0900

    :sparkles: Add component that checks viewport

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,42 +1,14 @@
 import React from "react";
-import styled from "styled-components";
 import { Title, Section } from "../index";
-import Airdnd from "./Airdnd";
-import GhostLeg from "./GhostLeg";
-import TimeToTravel from "./TimeToTravel";
+import ViewChecker from "./ViewChecker";
 
 const Project = () => {
   return (
     <Section id="project">
       <Title>PROJECT</Title>
-      <List>
-        <Item bottom="30rem">
-          <Airdnd />
-        </Item>
-        <Item bottom="20rem">
-          <GhostLeg />
-        </Item>
-        <Item bottom="10rem">
-          <TimeToTravel />
-        </Item>
-      </List>
+      <ViewChecker />
     </Section>
   );
 };
 
 export default Project;
-
-const List = styled.ul`
-  margin-top: 10rem;
-`;
-
-const Item = styled.li`
-  width: 100%;
-  position: relative;
-  max-height: 700px;
-  margin-bottom: ${({ bottom }) => bottom};
-
-  @media ${({ theme }) => theme.size.mobile} {
-    margin-bottom: 15rem;
-  }
-`;
---
+++ src/Components/Project/ViewChecker.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Airdnd from "./Airdnd";
+import GhostLeg from "./GhostLeg";
+import TimeToTravel from "./TimeToTravel";
+
+const ViewChecker = () => {
+  const [viewSize, setViewSize] = useState(window.innerWidth);
+  const getViewSize = () => setViewSize(window.innerWidth);
+  window.addEventListener("resize", getViewSize);
+
+  useEffect(() => {
+    console.log(viewSize);
+    return () => window.removeEventListener("resize", getViewSize);
+  }, [viewSize]);
+
+  return (
+    <>
+      <List>
+        <Item bottom="30rem">
+          <Airdnd />
+        </Item>
+        <Item bottom="20rem">
+          <GhostLeg />
+        </Item>
+        <Item bottom="10rem">
+          <TimeToTravel />
+        </Item>
+      </List>
+    </>
+  );
+};
+
+export default ViewChecker;
+
+const List = styled.ul`
+  margin-top: 10rem;
+`;
+
+const Item = styled.li`
+  width: 100%;
+  position: relative;
+  max-height: 700px;
+  margin-bottom: ${({ bottom }) => bottom};
+
+  @media ${({ theme }) => theme.size.mobile} {
+    margin-bottom: 15rem;
+  }
+`;
