f115d53c347c5b8b14700ad3c02cd55233e82488
kimfriendship <kfriendship94@gmail.com>
2020-12-24 14:06:19 +0900

    :dizzy: Add animation on mobile ver menu

--- src/Components/Global/Menu.js
+++ src/Components/Global/Menu.js
@@ -1,22 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import styled from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 import { Link } from "react-scroll";
 import { Close } from "@styled-icons/evaicons-solid/Close";
 
 const Menu = ({ isHome, menuState, closeMenu }) => {
-  if (!menuState) return null;
-
+  if (menuState === null) return null;
   return ReactDOM.createPortal(
-    <Wrapper>
+    <Wrapper menuState={menuState}>
       <Nav isHome={isHome}>
-        <Link smooth spy hashSpy to="home" activeClass="active" duration={800}>
+        <Link
+          onClick={closeMenu}
+          smooth
+          spy
+          hashSpy
+          to="home"
+          activeClass="active"
+          duration={800}
+        >
           HOME
         </Link>
-        <Link smooth spy hashSpy to="about" activeClass="active" duration={800}>
+        <Link
+          onClick={closeMenu}
+          smooth
+          spy
+          hashSpy
+          to="about"
+          activeClass="active"
+          duration={800}
+        >
           ABOUT
         </Link>
         <Link
+          onClick={closeMenu}
           smooth
           spy
           hashSpy
@@ -37,6 +53,24 @@ const Menu = ({ isHome, menuState, closeMenu }) => {
 
 export default Menu;
 
+const show = keyframes`
+0% {
+  transform: translate3d(100vw, 0, 0);
+}
+100% {
+  transform: translate3d(0, 0, 0);
+}
+`;
+
+const hide = keyframes`
+0% {
+  transform: translate3d(0, 0, 0);
+}
+100% {
+  transform: translate3d(100vw, 0, 0);
+}
+`;
+
 const Wrapper = styled.div`
   background-color: lightseagreen;
   width: 100vw;
@@ -45,6 +79,14 @@ const Wrapper = styled.div`
   top: 0;
   right: 0;
   z-index: 200;
+  ${({ menuState }) =>
+    menuState
+      ? css`
+          animation: ${show} 0.2s forwards;
+        `
+      : css`
+          animation: ${hide} 0.2s forwards;
+        `}
 `;
 
 const Nav = styled.nav`
@@ -82,8 +124,8 @@ const Button = styled.button`
   height: 3.5rem;
   border-radius: 50%;
   position: absolute;
-  top: 1rem;
-  right: 1rem;
+  top: 2rem;
+  right: 2rem;
 
   &:hover,
   &:focus {
--- src/Components/Global/MenuButtonContainer.js
+++ src/Components/Global/MenuButtonContainer.js
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import MenuButton from "./MenuButton";
 
 const MenuButtonContainer = () => {
-  const [menuState, setMenuState] = useState(false);
+  const [menuState, setMenuState] = useState(null);
   const openMenu = () => setMenuState(true);
-  const closeMenu = () => setMenuState(false);
+  const closeMenu = () => {
+    setMenuState(false);
+    setTimeout(() => setMenuState(null), 300);
+  };
 
   return <MenuButton menu={{ menuState, openMenu, closeMenu }} />;
 };
